1303247e04e25d44d6d2243adc06c652aef9fb96
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-02-12 11:21:30 -0300

    implementando autenticação pelas credenciais

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.authentication[:username] && password == Rails.application.credentials.authentication[:password]
+    end
+  end
 end
