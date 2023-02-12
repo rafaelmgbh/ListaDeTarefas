4a8a6b9910bc907994f565dbc0c2bb92a8103c2b
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-02-12 20:43:19 -0300

    teste commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for developmen
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
